917abcd91e7c538b9f398624602b798a64b388e9
Vlad Volkov <vlad.volkov@bndigital.co>
2021-03-03 12:18:39 +0200

    Updated versions, libraries, parameters

--- src/skaffold.ts
+++ src/skaffold.ts
@@ -1,6 +1,6 @@
 import { cacheDir } from '@actions/tool-cache'
 import { exec } from '@actions/exec'
-import { getInput, setFailed } from '@actions/core'
+import { getInput, setCommandEcho, setFailed } from '@actions/core'
 import { mkdirP } from '@actions/io'
 import { download, getBinDir, getHomeDir, getOsPlatform } from './index'
 import { join } from 'path'
@@ -44,14 +44,13 @@ async function run(): Promise<void> {
   const containerStructureTestVersion = getInput('container-structure-test-version')
   const skaffoldTestUrl = `https://github.com/GoogleContainerTools/skaffold/releases/download/v${skaffoldVersion}/skaffold-${platform}-amd64${suffix}`
   const containerStructureTestUrl = `https://storage.googleapis.com/container-structure-test/v${containerStructureTestVersion}/container-structure-test-${platform}-amd64`
-
   try {
     await mkdirP(skaffoldHomeDir)
     await download(skaffoldTestUrl, join(binDir, 'skaffold'))
     if (getInput('skip-tests') === 'false') {
       await download(containerStructureTestUrl, join(binDir, 'container-structure-test'))
     }
-    await exec('skaffold', getArgsFromInput())
+    await exec('skaffold', getArgsFromInput(), { env: { ACTIONS_ALLOW_UNSECURE_COMMANDS: 'true' } })
     await cacheDir(skaffoldHomeDir, 'skaffold', skaffoldVersion)
   } catch (error) {
     setFailed(error.message)
