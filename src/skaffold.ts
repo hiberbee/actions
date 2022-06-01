0cbd6d7dbbb54db37897bdaf6eb9363f2b71273c
Vlad Volkov <vlad.volkov@bndigital.co>
2022-06-01 23:06:38 +0300

    Output one more fix

--- src/skaffold.ts
+++ src/skaffold.ts
@@ -1,113 +1,114 @@
-import { exec } from "@actions/exec";
-import { setOutput, getInput, setFailed } from "@actions/core";
-import { mkdirP } from "@actions/io";
-import { download, getBinDir, getOsPlatform, getWorkspaceDir } from "./index";
-import { join } from "path";
+import { exec } from '@actions/exec'
+import { setOutput, getInput, setFailed } from '@actions/core'
+import { mkdirP } from '@actions/io'
+import { download, getBinDir, getOsPlatform, getWorkspaceDir } from './index'
+import { join } from 'path'
+import { ExecOptions } from '@actions/exec/lib/interfaces'
 
 const paramsArgumentsMap: Record<string, string> = {
-  ["insecure-registries"]: "insecure-registry",
-  ["kube-context"]: "kubeconfig",
-  ["skip-tests"]: "skip-tests",
-  [`cache-file`]: "cache-file",
-  cache: "cache-artifacts",
-  concurrency: "build-concurrency",
-  filename: "filename",
-  output: "output",
-  image: "build-image",
-  interactive: "interactive",
-  kubeconfig: "kubeconfig",
-  namespace: "namespace",
-  profile: "profile",
-  push: "push",
-  repository: "default-repo",
-  tag: "tag",
-  verbosity: "verbosity",
-};
+  ['insecure-registries']: 'insecure-registry',
+  ['kube-context']: 'kubeconfig',
+  ['skip-tests']: 'skip-tests',
+  [`cache-file`]: 'cache-file',
+  cache: 'cache-artifacts',
+  concurrency: 'build-concurrency',
+  filename: 'filename',
+  output: 'output',
+  image: 'build-image',
+  interactive: 'interactive',
+  kubeconfig: 'kubeconfig',
+  namespace: 'namespace',
+  profile: 'profile',
+  push: 'push',
+  repository: 'default-repo',
+  tag: 'tag',
+  verbosity: 'verbosity',
+}
 
-const workspaceDir = getWorkspaceDir();
-const platform = getOsPlatform();
+const workspaceDir = getWorkspaceDir()
+const platform = getOsPlatform()
 
-const binDir = getBinDir(workspaceDir);
-const skaffoldHomeDir = join(workspaceDir, ".skaffold");
+const binDir = getBinDir(workspaceDir)
+const skaffoldHomeDir = join(workspaceDir, '.skaffold')
 
 /**
  * @param {string} name
  * @param {string} version
  */
 function getBinaryUrl(name: string, version: string): string {
-  const extension = platform === "windows" ? ".exe" : "";
+  const extension = platform === 'windows' ? '.exe' : ''
 
-  return `https://storage.googleapis.com/${name}/releases/v${version}/${name}-${platform}-amd64${extension}`;
+  return `https://storage.googleapis.com/${name}/releases/v${version}/${name}-${platform}-amd64${extension}`
 }
 
 /**
  * @return {string[]}
  */
 function resolveArgsFromAction(): string[] {
-  return getInput("command") === ""
-    ? ["version"]
-    : getInput("command")
-        .split(" ")
+  return getInput('command') === ''
+    ? ['version']
+    : getInput('command')
+        .split(' ')
         .concat(
           Object.entries(paramsArgumentsMap)
             .map(([actionParam, skaffoldArg]) => {
-              return getInput(actionParam) !== "" ? `--${skaffoldArg}=${getInput(actionParam)}` : "";
+              return getInput(actionParam) !== '' ? `--${skaffoldArg}=${getInput(actionParam)}` : ''
             })
-            .filter((it) => it !== "")
-        );
+            .filter((it) => it !== ''),
+        )
 }
 
 type ImageBuildOutput = {
-  imageName: string;
-  tag: string;
-};
+  imageName: string
+  tag: string
+}
 
 type BuildOutput = {
-  builds: ImageBuildOutput[];
-};
+  builds: ImageBuildOutput[]
+}
 
 /**
  * Fix: https://github.com/hiberbee/github-action-skaffold/issues/14
  * @param {string[]} args
  */
 function filterOutputSkitTests(args: string[]) {
-  return getInput("output") || args.find((each) => each.startsWith("--output"))
-    ? args.filter((arg) => !arg.startsWith("--skip-tests"))
-    : args;
+  return getInput('output') || args.find((each) => each.startsWith('--output'))
+    ? args.filter((arg) => !arg.startsWith('--skip-tests'))
+    : args
 }
 
 async function run(): Promise<void> {
-  const skaffoldTUrl = getBinaryUrl("skaffold", getInput("skaffold-version"));
+  const skaffoldTUrl = getBinaryUrl('skaffold', getInput('skaffold-version'))
   const containerStructureTestUrl = getBinaryUrl(
-    "container-structure-test",
-    getInput("container-structure-test-version")
-  );
+    'container-structure-test',
+    getInput('container-structure-test-version'),
+  )
 
+  const options: ExecOptions = { cwd: getInput('working-directory') ?? workspaceDir }
   try {
-    await mkdirP(skaffoldHomeDir);
-    await download(skaffoldTUrl, join(binDir, "skaffold"));
-    if (!Boolean(getInput("skip-tests"))) {
-      await download(containerStructureTestUrl, join(binDir, "container-structure-test"));
+    await mkdirP(skaffoldHomeDir)
+    await download(skaffoldTUrl, join(binDir, 'skaffold'))
+    if (!Boolean(getInput('skip-tests'))) {
+      await download(containerStructureTestUrl, join(binDir, 'container-structure-test'))
     }
-    const args = filterOutputSkitTests(resolveArgsFromAction());
+    const args = filterOutputSkitTests(resolveArgsFromAction())
 
-    await exec("skaffold", args, {
-      cwd: getInput("working-directory") ?? workspaceDir,
-    }).then(() =>
-      exec("skaffold", ["build"].concat(args.slice(1).concat(["--quiet", "--output='{{json .}}'"])), {
+    await exec('skaffold', args, options).then(() =>
+      exec('skaffold', ['build'].concat(args.slice(1).concat(['--quiet', "--output='{{json .}}'"])), {
+        ...options,
         listeners: {
           stdout: (output) => {
-            const data: BuildOutput = JSON.parse(output.toString("utf8"));
-            setOutput("builds", JSON.stringify(data.builds));
+            const data: BuildOutput = JSON.parse(output.toString('utf8'))
+            setOutput('builds', JSON.stringify(data.builds))
           },
         },
-      })
-    );
+      }),
+    )
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
   } catch (error: any) {
-    setFailed(error.message);
+    setFailed(error.message)
   }
 }
 
 // noinspection JSIgnoredPromiseFromCall
-run();
+run()
