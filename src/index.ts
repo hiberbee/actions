ebab6200f1c9ab3b72217aabf55fca4f0b20898d
Vlad Volkov <vlad.volkov@bndigital.co>
2023-02-05 08:55:02 +0200

    Reinitialized repository with common actions package

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,38 @@
-import { basename, dirname, join } from "path";
-import { platform } from "os";
-import { downloadTool } from "@actions/tool-cache";
-import { mkdirP, mv } from "@actions/io";
-import { exec } from "@actions/exec";
-import { addPath, } from "@actions/core";
+// noinspection JSUnusedGlobalSymbols
+
+import { addPath } from '@actions/core'
+import { exec } from '@actions/exec'
+import { mkdirP, mv } from '@actions/io'
+import { downloadTool } from '@actions/tool-cache'
+import { platform } from 'os'
+import { basename, dirname, join } from 'path'
 
 export function getBinDir(rootDir: string): string {
-  return join(rootDir, "bin");
+  return join(rootDir, 'bin')
 }
 
 export function getOsPlatform(): string {
-  return platform() === "win32" ? "windows" : platform().toLowerCase();
+  return platform() === 'win32' ? 'windows' : platform().toLowerCase()
 }
 
+/**
+ * @returns {string} the path to the action's' working directory
+ */
 export function getWorkspaceDir(): string {
-  return process.env.GITHUB_WORKSPACE ?? join(__dirname, "..");
+  return process.env.GITHUB_WORKSPACE ?? join(process.cwd())
 }
 
 export async function download(url: string, destination: string): Promise<string> {
-  const downloadPath = await downloadTool(url);
-  const destinationDir = dirname(destination);
-  await mkdirP(destinationDir);
-  if (url.endsWith("tar.gz") || url.endsWith("tar") || url.endsWith("tgz")) {
-    await exec("tar", ["-xzf", downloadPath, `--strip=1`]);
-    await mv(basename(destination), destinationDir);
+  const downloadPath = await downloadTool(url)
+  const destinationDir = dirname(destination)
+  await mkdirP(destinationDir)
+  if (url.endsWith('tar.gz') || url.endsWith('tar') || url.endsWith('tgz')) {
+    await exec('tar', ['-xzf', downloadPath, `--strip=1`])
+    await mv(basename(destination), destinationDir)
   } else {
-    await mv(downloadPath, destination);
+    await mv(downloadPath, destination)
   }
-  await exec("chmod", ["+x", destination]);
-  addPath(destinationDir);
-  return downloadPath;
+  await exec('chmod', ['+x', destination])
+  addPath(destinationDir)
+  return downloadPath
 }
